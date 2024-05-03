aa0d304d8b2911e4d2c2fd0577f96ffda7e8a2fd
tanmay0502 <81376006+tanmay0502@users.noreply.github.com>
2024-05-03 07:33:09 +0530

    Connecion with socket success

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,40 @@ export default function Home() {
     fetchSid();
   }, []);
 
+  useEffect(() => {
+    if (sid) {
+      const socket = new WebSocket(`wss://fawss.pi42.com/socket.io/?EIO=4&transport=websocket&sid=${sid}`);
+
+      socket.onopen = function (event) {
+        console.log("WebSocket is open now.");
+        socket.send('2probe');
+      };
+
+      socket.onclose = function (event) {
+        console.log("WebSocket is closed now.");
+      };
+
+      socket.onerror = function (error) {
+        console.log(`WebSocket error: ${error}`);
+      };
+
+      socket.onmessage = function (event) {
+        console.log(`Data received from server: ${event.data}`);
+        if (event.data === '3probe') {
+          socket.send('5');
+          
+          //socket.send('42["subscribe",{params: ["btcinr@aggTrade", "btcinr@depth_0.1"]}]');
+          //socket.send('42["subscribe",{params: ["btcinr@markPrice", "btcinr@ticker"]}]');
+          //socket.send('42["subscribe", {params: ["btcinr@kline_15m"]}]')
+          socket.send('3');
+        }else{
+          console.log(`Data received from server: ${event.data}`);
+          socket.send('43["subscribe",{params: ["btcinr@aggTrade", "btcinr@depth_0.1"]}]');
+        }
+      };
+    }
+  }, [sid]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>SID: {sid}</p>
